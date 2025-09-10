3874721debd74c8087b4cf6bf39d5b6ba912c0cb
Solothiel <dste486@wgu.edu>
2025-09-10 06:49:36 -0600

    created the Weather_DB.py
    added sqlalchemy
    
    WeatherRecord → Class  → completed
    
    Process finished with exit code 0

--- Weather_DB.py
+++ Weather_DB.py
@@ -1,13 +1,13 @@
 from sqlalchemy import (Column, Float, Integer)
 from sqlalchemy.orm import declarative_base
 
-Base = declarative_base
+Base = declarative_base ()
 
 class WeatherRecord(Base):
     """ This sets the metadata and column names for the database that will be
         populated and used in main"""
     __tablename__ = "weather_data"
-    id = Column (Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     latitude = Column(Float)
     longitude = Column(Float)
     month = Column(Integer)
@@ -24,5 +24,6 @@ class WeatherRecord(Base):
     sum_precipitation = Column(Float)
     min_precipitation = Column(Float)
     max_precipitation = Column(Float)
+
 
 
