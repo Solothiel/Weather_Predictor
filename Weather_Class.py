c64df7f8146cdd3a31f6ccd5f57724abf0ae8ad3
Solothiel <dste486@wgu.edu>
2025-09-09 20:09:46 -0600

    created the Weather_Class.py
    WeatherData  → class → completed
    fetch_weather_single_day → method → completed
    
    Finished off re-ran code.
    Process finished with exit code 0

--- Weather_Class.py
+++ Weather_Class.py
@@ -81,3 +81,38 @@ class WeatherData:
             self.five_year_max_precip = max(precs)
         else:
             print("Precipitation data missing.")
+
+    def fetch_weather_single_day(self, year, variable):
+        """ This creates the single day variable that is used in the api.
+            This is method is used to help generate information on the other methods
+            with the class.
+            """
+        date_str = f"{year}-{self.month:02d}-{self.day:02d}"
+        latitude_str = f"{self.loc_latitude:.2f}"
+        longitude_str = f"{self.loc_longitude:.2f}"
+
+        url = (
+            f"https://archive-api.open-meteo.com/v1/archive?"
+            f"latitude={latitude_str}&longitude={longitude_str}"
+            f"&start_date={date_str}&end_date={date_str}"
+            f"&daily={variable}"
+            f"&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch"
+            f"&timezone=auto"
+        )
+        #This is the query responses and it deciphers the error codes that occur.
+        response = requests.get(url)
+        if response.status_code == 200:
+            json_data = response.json()
+            daily = json_data.get("daily", {})
+            values = daily.get(variable, [])
+            if values:
+                return values[0]
+            else:
+                return None
+        elif response.status_code == 404:
+            print(f"Error 404: Data not found for {date_str}.")
+        elif response.status_code == 500:
+            print(f"Error 500: Server error for {date_str}. Try again later.")
+        else:
+            print(f"Error {response.status_code}: An unknown error occurred for {date_str}.")
+        return None
